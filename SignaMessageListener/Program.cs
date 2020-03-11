40f577c0a1606540f0ff58aba20ed38dda5f3b81
Tarique Memon <tmemon@chemcentre.wa.gov.au>
2020-03-11 13:22:54 +0800

    minor changes for signa

--- SignaMessageListener/Program.cs
+++ SignaMessageListener/Program.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
 using System.Linq;
+using System.Threading;
 using ZetaIpc.Runtime.Client;
 
 namespace SignaMessageListener
@@ -19,25 +20,28 @@ namespace SignaMessageListener
 
           private static void SendMessage(string arguments)
         {
+            if (string.IsNullOrEmpty(arguments)) return;
+
             var c = new IpcClient();
             c.Initialize(12345);
 
-            c.Send(arguments);            
+            c.Send(arguments);
         }
 
         static void InitializeProcess()
         {
             Process[] p;
-            p = Process.GetProcessesByName("SignaShell");
+            p = Process.GetProcessesByName("SIGNA.WPF");
 
             if (p.Count() == 0)
             {
                 using (var newProcess = new Process())
                 {
-                    newProcess.StartInfo.FileName = "C:\\Development\\Training\\SignaShell\\SignaShell\\bin\\Debug\\SignaShell.exe";
+                    newProcess.StartInfo.FileName = "C:\\Development\\CCWA\\Signa-UnifiedLab-OneLIMS Demo\\WPF\\bin\\Debug\\SIGNA.WPF.exe";
                     newProcess.StartInfo.CreateNoWindow = true;
                     newProcess.StartInfo.UseShellExecute = false;
                     newProcess.Start();
+                    Thread.Sleep(10000);
                 }
             }
         }
